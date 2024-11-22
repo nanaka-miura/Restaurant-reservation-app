c1d34768010ebb72a9321275f59b7beac52da467
nanaka miura <7ca.3ura@gmail.com>
2024-11-22 10:39:48 +0900

    QRコード昨日の実装

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command("reminder:send")->dailyAt('14:00');
+        $schedule->command('reminder:send')->dailyAt('11:50');
     }
 
     /**
---
+++ src/database/seeders/ReservationsTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ReservationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $userIds = DB::table('users')->pluck('id');
+        $shopIds = DB::table('shops')->pluck('id');
+
+        $startDate = strtotime('2024-01-01');
+        $endDate = strtotime('2024-12-31');
+
+        $reservations = [];
+        
+        for ($i = 0; $i < 15; $i++) {
+            $randomTimestamp = rand($startDate, $endDate);
+            $randomDate = date('Y-m-d', $randomTimestamp);
+            
+            $reservations[] = [
+                'user_id' => $userIds->random(),
+                'shop_id' => $shopIds->random(),
+                'date' => $randomDate,
+                'time' => collect(['17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00'])->random(),
+                'number' => collect(['1人', '2人', '3人', '4人', '5人', '6人', '7人', '8人', '9人', '10人'])->random(),
+            ];
+        }
+
+        foreach ($reservations as $reservation) {
+            DB::table('reservations')->insert($reservation);
+        }
+    }
+}
